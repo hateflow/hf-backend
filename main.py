e0c3cf2e2f9f3e827512692d73fda23bab41ed68
Jakob Schödl <jschoedl04@gmail.com>
2021-05-26 17:40:42 +0200

    restructure for olga

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import request
+import requests
+from flask import Flask, request
+from flask_cors import CORS
+from git import Repo
 
 try:
     from . import naive_bias_predict
@@ -6,22 +9,50 @@ except ImportError:
     import naive_bias_predict
 
 
-def main(*args, **kwargs) -> (dict, int):
-    res = {
-        'results': dict(),
-        'errors': [],
-        'warnings': [],
-    }
+def main():
+    app = Flask(__name__)
+    CORS(app)
 
-    params: dict = request.args  # parameters passed in the HTTP request
-    text = params.get("text")
-    if text is None:
-        res['errors'] = "no text submitted"
-        return res, 400
+    @app.route("/git/neseps/webhook", methods=["POST"])
+    def update_git():
+        return
+        Repo("/home/JSchoedl/site/neseps").remotes.origin.pull()
+        Repo("/home/JSchoedl/site/neseps-docs").remotes.origin.pull()
+        # Repo.clone_from("https://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@github.com/jschoedl/neseps.git", "neseps")
 
-    results, errors, warnings = naive_bias_predict.predict([text], probabilities=params.get("probabilities"))
-    res['results'].update(results)
-    res['errors'].append(errors)
-    res['warnings'].append(warnings)
+        username = 'JSchoedl'
+        token = '06b26af687480abff2701135ce4906fde5cb3af9'
+        domain_name = "JSchoedl.eu.pythonanywhere.com"
 
-    return res, 200
+        res = requests.post(
+            f"https://eu.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/",
+            headers={'Authorization': f'Token {token}'}
+        )
+        return res
+
+    @app.route("/api/<api_request>")
+    def neseps_api(api_request):
+        res = {
+            'results': dict(),
+            'errors': [],
+            'warnings': [],
+        }
+
+        params: dict = request.args  # parameters passed in the HTTP request
+        text = params.get("text")
+        if text is None:
+            res['errors'] = "no text submitted"
+            return res, 400
+
+        results, errors, warnings = naive_bias_predict.predict([text], probabilities=params.get("probabilities"))
+        res['results'].update(results)
+        res['errors'].append(errors)
+        res['warnings'].append(warnings)
+
+        return res, 200
+
+    return app
+
+app = main()
+if __name__ == "__main__":
+    app.run()
