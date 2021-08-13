20d196b9e8711d2888c648e36ed9568230967329
Jakob Schödl <jschoedl04@gmail.com>
2021-08-13 14:05:49 +0200

    custom thresholds performance improvements

--- naive_bias.py
+++ naive_bias.py
@@ -4,7 +4,9 @@ from multiprocessing import Pool
 
 import nltk
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import MultinomialNB
 
 nltk.download('punkt')
@@ -40,19 +42,50 @@ def text_preprocess(data, stemmer):
     return res
 
 
-def fit_model(args, max_features=5000):
-    x_train, y_train, x_test, y_test, label_index = args
-    vectorizer = CountVectorizer(max_features=max_features)
-    x_train_vectorized = vectorizer.fit_transform(x_train)
-    x_test_vectorized = vectorizer.transform(x_test)
+def fit_model(args):
+    x_train_vectorized, y_train, label_index = args
 
     model = MultinomialNB()
     model.fit(x_train_vectorized, y_train[label_index])
 
     with open(f"models/{LABELS[label_index]}.pickle", "wb") as f:
-        pickle.dump((vectorizer, model), f)
+        pickle.dump(model, f)
 
-    return model.score(x_test_vectorized, y_test[label_index])
+    return model  # .score(x_test_vectorized, y_test[label_index])
+
+
+def plot_threshold_scores(label_index, model: MultinomialNB, x_test_vectorized, y_test):
+    y_test = y_test[label_index]
+
+    true_scores = dict()
+    false_scores = dict()
+
+    x_test_true = x_test_vectorized[y_test == True]
+    y_test_true = y_test[y_test == True]
+    x_test_false = x_test_vectorized[y_test == False]
+    y_test_false = y_test[y_test == False]
+
+    for i in range(1001):
+        threshold = i / 1000
+
+        y_true_pred = (model.predict_proba(x_test_true)[:, 1] > threshold) * 1
+        true_scores[threshold] = accuracy_score(y_test_true, y_true_pred)
+
+        y_false_pred = (model.predict_proba(x_test_false)[:, 1] > threshold) * 1
+        false_scores[threshold] = accuracy_score(y_test_false, y_false_pred)
+
+    plt.cla()
+    plt.plot(true_scores.keys(), true_scores.values(), label=LABELS[label_index])
+    plt.plot(false_scores.keys(), false_scores.values(), label=f"not {LABELS[label_index]}")
+
+    best_threshold = max(true_scores.keys(), key=lambda k: true_scores[k] + false_scores[k])
+    best_score = (true_scores[best_threshold] + false_scores[best_threshold]) / 2
+    plt.plot([best_threshold, best_threshold], [0, 1],
+             label=f"best score: {best_score:.3f} using threshold {best_threshold}")
+
+    plt.legend()
+    plt.title(LABELS[label_index])
+    plt.savefig(LABELS[label_index])
 
 
 def main():
@@ -63,11 +96,17 @@ def main():
     # x_train = text_preprocess(x_train, stemmer)
     # x_test = text_preprocess(x_test_start, stemmer)
 
+    vectorizer = CountVectorizer(max_features=5000)
+    x_train_vectorized = vectorizer.fit_transform(x_train)
+    x_test_vectorized = vectorizer.transform(x_test_start)
+    with open("models/vectorizer.pickle", "wb") as f:
+        pickle.dump(vectorizer, f)
+
     with Pool() as pool:
         new_scores = pool.map_async(fit_model,
-                                    [(x_train, y_train, x_test_start, y_test_start, i) for i in range(len(LABELS))])
-        for i, score in enumerate(new_scores.get()):
-            print(f"{LABELS[i]}: {score}")
+                                    [(x_train_vectorized, y_train, label_index) for label_index in range(len(LABELS))])
+        for label_index, model in enumerate(new_scores.get()):
+            plot_threshold_scores(label_index, model, x_test_vectorized, y_test_start)
 
 
 if __name__ == '__main__':
--- naive_bias_predict.py
+++ naive_bias_predict.py
@@ -6,26 +6,50 @@ from typing import List
 nltk.download('punkt')
 LABELS = "toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"
 CURRENT_DIR = "/".join(__file__.split("/")[:-1])
+DEFAULT_THRESHOLDS = {
+    'identity_hate': 0.011,
+    'insult': 0.062,
+    'obscene': 0.099,
+    'severe_toxic': 0.011,
+    'threat': 0.008,
+    'toxic': 0.104,
+}
+
+previous_errors = []
+models = dict()
+for label in LABELS:
+    try:
+        with open(f"{CURRENT_DIR}/models/{label}.pickle", "rb") as f:
+            models[label] = pickle.load(f)
+    except OSError as e:
+        previous_errors.append(f"could not load model for label '{label}': {e}")
+    except Exception as e:
+        previous_errors.append(f"unknown exception while loading label '{label}': {e}")
+
+try:
+    with open(f"{CURRENT_DIR}/models/vectorizer.pickle", "rb") as f:
+        vectorizer = pickle.load(f)
+except Exception as e:
+    previous_errors.append(f"unknown exception while loading the vectorizer: {e}")
 
 
-def predict(data: List[str], probabilities=None) -> (dict, list, list):
-    results, errors, warnings = dict(), [], []
+def predict(data: List[str], probabilities: bool = None, thresholds: dict = None) -> (dict, list, list):
+    results, errors, warnings = dict(), previous_errors.copy(), []
+
+    if thresholds is None:
+        thresholds = dict()
+    elif probabilities:
+        warnings.append("The given thresholds will be ignored since probabilities were requested. "
+                        "Set 'probabilities' to False for using thresholds.")
 
     for label in LABELS:
         try:
-            try:
-                with open(f"{CURRENT_DIR}/models/{label}.pickle", "rb") as f:
-                    vectorizer, model = pickle.load(f)
-            except OSError as e:
-                errors.append(f"could not load model for label '{label}': {e}")
-            except Exception as e:
-                errors.append(f"unknown exception while loading label '{label}': {e}")
-            else:
-                if probabilities:
-                    y_pred = model.predict_proba(vectorizer.transform(data))[0][0]
-                else:
-                    y_pred = model.predict(vectorizer.transform(data))[0]
-                results[label] = y_pred.item()
+            model = models[label]
+            y_pred = model.predict_proba(vectorizer.transform(data))[:, 1]
+            if not probabilities:
+                threshold = thresholds.get(label) or DEFAULT_THRESHOLDS[label]
+                y_pred = y_pred > threshold
+            results[label] = y_pred.item()
         except Exception as e:
             errors.append(f"unknown exception while processing label '{label}': {e}")
     return results, errors, warnings
