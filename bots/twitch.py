75fd334894e7fb5c656647c86cf235f75b501151
Jakob Schödl <jschoedl04@gmail.com>
2021-12-21 13:22:24 +0100

    standardize bots

--- bots/twitch.py
+++ bots/twitch.py
@@ -1,53 +1,21 @@
-import json
-import logging
 import os
 
-import requests
 from dotenv import load_dotenv
 from twitchio.ext import commands
+
+from bots_base import process_message
 
 API_URL = "http://api.hateflow.de/"
 
 load_dotenv()
 bot = commands.Bot(
     # set up the bot
-    token=os.getenv('TMI_TOKEN'),
-    client_id=os.getenv('CLIENT_ID'),
-    nick=os.getenv('BOT_NICK'),
-    prefix=os.getenv('BOT_PREFIX'),
-    initial_channels=[os.getenv('CHANNEL')]
+    token=os.getenv('T_TMI_TOKEN'),
+    client_id=os.getenv('T_CLIENT_ID'),
+    nick=os.getenv('T_BOT_NICK'),
+    prefix=os.getenv('T_BOT_PREFIX'),
+    initial_channels=[os.getenv('T_CHANNEL')]
 )
-
-
-def warning_text(context, properties: list) -> str:
-    assert properties
-
-    names = {
-        'identity_hate': "identity hate",
-        'insult': "insult",
-        'obscene': "obscenity",
-        'severe_toxic': "severe toxicity",
-        'threat': "threat",
-        'toxic': "toxicity",
-    }
-    properties = [names.get(i) or i for i in properties]
-
-    detected_properties = " and ".join(properties[-2:])
-    if len(properties) > 2:
-        detected_properties = f"{', '.join(properties[:-2])}, {detected_properties}"
-
-    text = f"Hey @{context.author.name}, I detected {detected_properties}.\n\n" \
-           f"I am a bot developed by HateFlow. This action was performed automatically."
-    return text
-
-
-def check_comment(comment) -> (str, dict):
-    response = requests.get(f"{API_URL}simpleCheck?text={comment}")
-    if response.status_code != 200:
-        return f"Connection to {API_URL} failed with status code {response.status_code}.", dict()
-    else:
-        data = json.loads(response.text)
-        return "", data['results']
 
 
 @bot.event
@@ -57,27 +25,15 @@ async def event_ready():
 
 @bot.event
 async def event_message(context):
-    """Runs every time a message is sent in chat."""
-    print("---------new comment---------")
-    print(context.content)
-
     # make sure the bot ignores itself and the streamer
     if context.author.name.lower() == os.getenv('BOT_NICK').lower():
         return
-    error, result = check_comment(context.content)
 
-    if error:
-        logging.error(error)
-    else:
-        properties = []
-        for key, value in result.items():
-            if value == 1:
-                properties.append(key)
-
-        if properties:
-            text = warning_text(context, properties)
-            print(text)
-            # await ctx.channel.send(text)
+    warning = process_message(context.content)
+    if warning:
+        response = f"Hey @{context.author.name}, {warning}"
+        print(response)
+        # await ctx.channel.send(response)
 
 
 if __name__ == "__main__":
